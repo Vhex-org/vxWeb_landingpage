3a81ef6c42920360d3f4be5ec29fa321537cfcad
Pierre ADER <pierre.ader@epitech.eu>
2021-11-09 17:52:55 +0100

    [ADD] add css and html to account.js to display the page and new functionnality : toggle password for each input type password from the account page

--- src/components/Account.js
+++ src/components/Account.js
@@ -18,14 +18,43 @@ function Account() {
         toHide.classList.remove("active-account-bloc");
     }
 
+    function toggleCreatePassword() {
+        const togglePassword = document.querySelector('#toggleCreatePassword');
+        const password = document.querySelector('#createPassword');
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        password.setAttribute('type', type);
+        togglePassword.classList.toggle('bi-eye');
+    }
+
+    function toggleCreateConfirmPassword() {
+        const togglePassword = document.querySelector('#toggleCreateConfirmPassword');
+        const password = document.querySelector('#createConfirmPassword');
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        password.setAttribute('type', type);
+        togglePassword.classList.toggle('bi-eye');
+    }
+
+    function toggleLoginPassword() {
+        const togglePassword = document.querySelector('#toggleLoginPassword');
+        const password = document.querySelector('#loginPassword');
+        const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+
+        password.setAttribute('type', type);
+        togglePassword.classList.toggle('bi-eye');
+    }
+
     return (
         <div className="account-content">
             <div className="account-container create-account-content active-account-bloc" onMouseOver={signInToSignUp}>
                 <h1 className="account-title account-sign-up">Sign up</h1>
                 <div className="account-input-container">
                     <input placeholder="Email"></input>
-                    <input type="password" placeholder="Password"></input>
-                    <input type="password" placeholder="Confirm Password"></input>
+                    <input type="password" placeholder="Password" id="createPassword"></input>
+                    <i className="account-pw bi bi-eye-slash" id="toggleCreatePassword" onClick={toggleCreatePassword}></i>
+                    <input type="password" placeholder="Confirm Password" id="createConfirmPassword"></input>
+                    <i className="account-pw bi bi-eye-slash" id="toggleCreateConfirmPassword" onClick={toggleCreateConfirmPassword}></i>
                 </div>
                 <button className="account-send-form">CONFIRMER</button>
             </div>
@@ -33,7 +62,8 @@ function Account() {
                 <h1 className="account-title account-sign-in">Sign in</h1>
                 <div className="account-input-container">
                     <input placeholder="Email"></input>
-                    <input type="password" placeholder="Password"></input>
+                    <input type="password" placeholder="Password" id="loginPassword"></input>
+                    <i className="account-pw bi bi-eye-slash" id="toggleLoginPassword"  onClick={toggleLoginPassword}></i>
                 </div>
                 <button className="account-send-form">CONFIRMER</button>
             </div>
