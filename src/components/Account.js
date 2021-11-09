829b25b094befb66b431f962fa1d00328ba4c389
Pierre ADER <pierre.ader@epitech.eu>
2021-11-09 16:07:54 +0100

    [ADD] add html css js to account page, gestion of focus (js), no gestion of send info

--- src/components/Account.js
+++ src/components/Account.js
@@ -2,9 +2,41 @@ import '../styles/global.css';
 import '../styles/Account.css';
 
 function Account() {
+    function signInToSignUp() {
+        var toShow = document.querySelector(".create-account-content");
+        var toHide = document.querySelector(".login-account-content");;
+
+        toShow.classList.add("active-account-bloc");
+        toHide.classList.remove("active-account-bloc");
+    }
+
+    function signUpToSignIn() {
+        var toShow = document.querySelector(".login-account-content");
+        var toHide = document.querySelector(".create-account-content");;
+
+        toShow.classList.add("active-account-bloc");
+        toHide.classList.remove("active-account-bloc");
+    }
+
     return (
-        <div className="Account-content">
-            
+        <div className="account-content">
+            <div className="account-container create-account-content active-account-bloc" onMouseOver={signInToSignUp}>
+                <h1 className="account-title account-sign-up">Sign up</h1>
+                <div className="account-input-container">
+                    <input placeholder="Email"></input>
+                    <input type="password" placeholder="Password"></input>
+                    <input type="password" placeholder="Confirm Password"></input>
+                </div>
+                <button className="account-send-form">CONFIRMER</button>
+            </div>
+            <div className="account-container login-account-content" onMouseOver={signUpToSignIn}>
+                <h1 className="account-title account-sign-in">Sign in</h1>
+                <div className="account-input-container">
+                    <input placeholder="Email"></input>
+                    <input type="password" placeholder="Password"></input>
+                </div>
+                <button className="account-send-form">CONFIRMER</button>
+            </div>
         </div>
     );
 }
