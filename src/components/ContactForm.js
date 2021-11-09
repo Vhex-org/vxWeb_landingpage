6a4595523b85fe6a8f3a99b763551ffbefdfdeae
Pierre ADER <pierre.ader@epitech.eu>
2021-11-09 14:05:42 +0100

    [ADD] add contact page without form, just css html

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,10 +1,31 @@
 import '../styles/global.css';
-//import '../styles/ContactForm.css';
+import '../styles/ContactForm.css';
 
 function ContactForm() {
     return (
-        <div>
-            <p>COUCOU</p>
+        <div className="ContactForm-content">
+            <div className="contact-container">
+                <h1 className="contactform-title first-letter-g">Contact</h1>
+                <select className="contact-select-reason">
+                    <option>Option 1</option>
+                    <option>Option 2</option>
+                </select>
+                <div className="contact-left-right-container">
+                    <div className="contact-left-container">
+                        <input className="first-letter-g contact-first-name" placeholder="Prénom *"></input>
+                        <input className="first-letter-g contact-last-name" placeholder="NOM *"></input>
+                        <input placeholder="Email *"></input>
+                        <div className="contact-agreement">
+                            <input type="checkbox"/>
+                            <p>J'accepte de partager mes informations avec Vhex</p>
+                        </div>
+                    </div>
+                    <div className="contact-right-container">
+                        <textarea className="contact-input-msg" placeholder="Comment pouvons-nous vous aider ?"></textarea>
+                    </div>
+                </div>
+                <button className="contact-send-form">CONFIRMER</button>
+            </div>
         </div>
     );
 }
